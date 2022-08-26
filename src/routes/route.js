bfb95d0e1f7655ecae05ea26ac46ab56fe395a1a
Bhuwan Singh Samant <107810689+Daarak-Sedo@users.noreply.github.com>
2022-08-26 06:57:08 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -9,13 +9,7 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-
-
-
 router.post("/createBook", BookController.createBook  )
-
-
-
 
 // router.post("/createUser", UserController.createUser  )
 // router.get("/getUsersData", UserController.getUsersData)
@@ -44,19 +38,10 @@ router.post("/createBook", BookController.createBook  )
 // router.get('/termsAndConditions',  UserController.termsAndConditions)
 // router.get('/register',  UserController.register)
 
-
-
-
-
 router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
-
-
 
 // router.get("/basicRoute2", commonMW.mid1, UserController.basicCode2)
 // router.get("/basicRoute3", commonMW.mid2, UserController.basicCode3)
 // router.get("/basicRoute4", commonMW.mid1, commonMW.mid4, UserController.basicCode4)
-
-
-
 
 module.exports = router;
