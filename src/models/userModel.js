fe6f10337128e325ee9d84814171d8b60bd339bc
Bhuwan Singh Samant <107810689+Daarak-Sedo@users.noreply.github.com>
2022-08-28 02:25:03 +0530

    Update userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,31 +1,19 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
-        type: String,
-        unique: true,
-        required: true
+const userSchema = new mongoose.Schema({
+    name: String,
+    balance: {
+        type: Number,
+        default: 100
     },
-    emailId: String,
+    address: String,
+    age: Number,
     gender: {
         type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+        enum: ["male", "female", "other"]
     },
-    age: Number,
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
+    isFreeAppUser: {type:Boolean}
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema) //users
 
-
-
-// String, Number
-// Boolean, Object/json, array
+module.exports = mongoose.model('User', userSchema);
